0dfc52fe67736fa9dabc75b7da50a371fa2648ea
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-08-03 02:58:02 +0100

    Simplify distributed method

--- decks/nets/build.py
+++ decks/nets/build.py
@@ -31,27 +31,5 @@ def build_net(ds):
 
 def build_dist_net(ds, strategy):
     with strategy.scope():
-        enc = text_encoder(ds, 2500)
-        model = tf.keras.Sequential(
-            [
-                enc,
-                tf.keras.layers.Embedding(
-                    input_dim=len(enc.get_vocabulary()),
-                    output_dim=64,
-                    mask_zero=True,
-                ),
-                tf.keras.layers.Dropout(0.8),
-                tf.keras.layers.Bidirectional(tf.keras.layers.GRU(64)),
-                tf.keras.layers.Dropout(0.7),
-                tf.keras.layers.Dense(64, activation="relu"),
-                tf.keras.layers.Dropout(0.6),
-                tf.keras.layers.Dense(6, activation="softmax"),
-            ]
-        )
-        model.compile(
-            loss="sparse_categorical_crossentropy",
-            optimizer="adam",
-            metrics=["accuracy"],
-        )
-
+        model = build_net(ds)
     return model
