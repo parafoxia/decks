1b8299adc13a516d21880a16d0c6f71cd43e605f
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-08-06 21:18:35 +0100

    Add GoEmotions stuff

--- decks/data.py
+++ decks/data.py
@@ -20,6 +20,24 @@ def load_carer(batch_size):
     return datasets
 
 
+def load_goemotions(batch_size):
+    df = pd.read_csv("goemotions.csv", index_col=0)
+    train_df = df[df["split"] == 0]
+    test_df = df[df["split"] == 1]
+    val_df = df[df["split"] == 2]
+
+    datasets = []
+
+    for df in train_df, test_df, val_df:
+        features = df.iloc[:, 0]
+        labels = df.iloc[:, 2:]
+        ds = tf.data.Dataset.from_tensor_slices((features.values, labels.values))
+        ds = ds.batch(batch_size, drop_remainder=True).prefetch(tf.data.AUTOTUNE)
+        datasets.append(ds)
+
+    return datasets
+
+
 if __name__ == "__main__":
-    train_ds, test_ds, val_ds = load_carer(64)
+    train_ds, test_ds, val_ds = load_goemotions(64)
     print(len(train_ds), len(test_ds), len(val_ds))
--- decks/nets/build.py
+++ decks/nets/build.py
@@ -3,19 +3,42 @@ import tensorflow as tf
 from decks.nets.utils import text_encoder
 
 
+# def build_net(ds):
+#     enc = text_encoder(ds, 2_500)
+#     model = tf.keras.Sequential(
+#         [
+#             enc,
+#             tf.keras.layers.Embedding(
+#                 input_dim=len(enc.get_vocabulary()),
+#                 output_dim=64,
+#                 mask_zero=True,
+#             ),
+#             tf.keras.layers.Bidirectional(tf.keras.layers.GRU(64)),
+#             tf.keras.layers.Dense(64, activation="relu"),
+#             tf.keras.layers.Dense(6, activation="softmax"),
+#         ]
+#     )
+#     model.compile(
+#         loss="sparse_categorical_crossentropy",
+#         optimizer=tf.keras.optimizers.Adam(1e-4),
+#         metrics=["accuracy"],
+#     )
+#     return model
+
+
 def build_net(ds):
-    enc = text_encoder(ds, 10_000)
+    enc = text_encoder(ds, 2_500)
     model = tf.keras.Sequential(
         [
             enc,
             tf.keras.layers.Embedding(
                 input_dim=len(enc.get_vocabulary()),
-                output_dim=128,
+                output_dim=64,
                 mask_zero=True,
             ),
-            tf.keras.layers.Bidirectional(tf.keras.layers.GRU(128)),
+            tf.keras.layers.Bidirectional(tf.keras.layers.GRU(64)),
             tf.keras.layers.Dense(64, activation="relu"),
-            tf.keras.layers.Dense(6, activation="softmax"),
+            tf.keras.layers.Dense(28, activation="sigmoid"),
         ]
     )
     model.compile(
