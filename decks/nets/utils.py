5f6dc6ede0668157b7b80e2b08481525cb1664e1
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-08-09 03:17:29 +0100

    Perform better standardisation on inputs

--- decks/nets/utils.py
+++ decks/nets/utils.py
@@ -1,9 +1,22 @@
+import re
+
 import tensorflow as tf
+from stopwordsiso import stopwords
+
+
+def _standardise(inputs):
+    x = tf.strings.lower(inputs)
+    x = tf.strings.regex_replace(x, "[^a-z0-9 ]", "")
+    x = tf.strings.regex_replace(x, " +", " ")
+    sw = [re.sub("[^a-z0-9]", "", w) for w in stopwords("en")]
+    x = tf.strings.regex_replace(x, r"\b(" + r"|".join(sw) + r")\b\s*", " ")
+    return x
 
 
 def text_encoder(ds, max_tokens):
     enc = tf.keras.layers.experimental.preprocessing.TextVectorization(
-        max_tokens=max_tokens
+        max_tokens=max_tokens,
+        standardize=_standardise,
     )
     enc.adapt(ds.map(lambda text, _: text))
     return enc
