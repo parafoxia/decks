ad3d8cee0ade46d70eb99a4a28004dfc6e4d3cff
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-08-15 21:57:41 +0100

    Add class weight calculations for Carer dataset

--- decks/data.py
+++ decks/data.py
@@ -3,6 +3,17 @@ import pandas as pd
 import tensorflow as tf
 
 
+def _calculate_weights_carer(labels):
+    samples = np.zeros(labels.max() + 1)
+
+    for i, count in labels.value_counts().items():
+        samples[i] = count
+
+    weights = np.max(samples) / samples
+    # weights = np.interp(weights, (weights.min(), weights.max()), (1, 10))
+    return {i: w for i, w in enumerate(weights)}
+
+
 def load_carer(batch_size):
     df = pd.read_csv("carer.csv", index_col=0)
     train_df = df[df["split"] == 0]
@@ -11,17 +22,21 @@ def load_carer(batch_size):
 
     datasets = []
 
-    for df in train_df, test_df, val_df:
+    for i, df in enumerate((train_df, test_df, val_df)):
         features = df.iloc[:, 0]
         labels = df.iloc[:, 1]
+
+        if i == 0:
+            weights = _calculate_weights_carer(labels)
+
         ds = tf.data.Dataset.from_tensor_slices((features.values, labels.values))
         ds = ds.batch(batch_size, drop_remainder=True).prefetch(tf.data.AUTOTUNE)
         datasets.append(ds)
 
-    return datasets, None
+    return datasets, weights
 
 
-def _calculate_weights(labels):
+def _calculate_weights_goemotions(labels):
     samples = np.zeros(len(labels.columns))
 
     for i, col in enumerate(labels.columns):
@@ -45,7 +60,7 @@ def load_goemotions(batch_size):
         labels = df.iloc[:, 2:]
 
         if i == 0:
-            weights = _calculate_weights(labels)
+            weights = _calculate_weights_goemotions(labels)
 
         ds = tf.data.Dataset.from_tensor_slices((features.values, labels.values))
         ds = ds.batch(batch_size, drop_remainder=True).prefetch(tf.data.AUTOTUNE)
@@ -58,3 +73,4 @@ if __name__ == "__main__":
     (train_ds, test_ds, val_ds), weights = load_carer(128)
     print(len(train_ds), len(test_ds), len(val_ds))
     print(len(train_ds) * 128)
+    print(weights)
